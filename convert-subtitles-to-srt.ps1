e48ac97fad958abd33ce3a7663054e790bd4e5c7
Joppeur <joukosirkka@gmail.com>
2019-05-09 18:02:49 +0900

    Add files via upload
    Inital commit.

---
+++ convert-subtitles-to-srt.ps1
@@ -0,0 +1,53 @@
+﻿$language = "eng"
+
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
+$FileBrowser.Multiselect = $true
+$FileBrowser.InitialDirectory = 'X:\'
+$FileBrowser.title = "Select files to process"
+$FileBrowser.Filter = "Matroska container (*.mkv)|*.mkv|All files (*.*)|*.*"
+$result = $FileBrowser.ShowDialog()
+if($result -eq "Cancel") {
+    exit
+}
+
+Write-Host "`n`n`n`n`n`n`n`n`n"
+Write-Host "ffmpeg subtitle extraction started."
+
+#Write-Host $FileBrowser.FileNames
+#$SrtFileNames = $FileBrowser.Filenames -Replace "\.[^\.]*$",".$language.srt"
+$SrtFileNames = New-Object Collections.Generic.List[String]
+
+$filesMoved = 1
+ForEach($file in $FileBrowser.FileNames){
+    $calc = ($filesMoved/$FileBrowser.FileNames.Count*100)
+    Write-Progress -Activity "Converting subtitles ..." -Status ("{0}/{1}" -f $filesMoved, $FileBrowser.FileNames.Count) -PercentComplete $calc  -CurrentOperation $file
+    $newName = $file -Replace "\.[^\.]*$",".$language.srt"
+	# change flag -y to -n to disable automatic overwriting
+    ffmpeg -hide_banner -loglevel error -i $file -map 0:s:0 "$newName" -y
+    $SrtFileNames.Add($newName)
+    $filesMoved++
+}
+
+# This is the best way I could figure how to get the folder path
+# Regexes the file name off the last processed file's full path.
+$folderPath = $file
+$folderPath = $folderPath -Replace "\\[^\\]*$"
+Set-Location -LiteralPath $folderPath
+
+Write-Host ".srt cleanup started."
+
+$filesMoved = 1
+ForEach($file in $SrtFileNames) {
+    # Progress Bar
+    $calc = ($filesMoved/$SrtFileNames.Count*100)
+    Write-Progress -Activity "Cleaning .srt files ..." -Status ("{0}/{1}" -f $filesMoved, $SrtFileNames.Count) -PercentComplete $calc  -CurrentOperation $file 
+    (Get-Content -Encoding UTF8 -LiteralPath "$file") -Replace '( <(.*?)> )' | Out-File -LiteralPath "$file"
+	#regex options. First on removes font sizes and bolding, 2nd one removes all font formatting.
+    #( size="(.*?))"|(<b(.*?)>)|(</b(.*?)>)
+    #"<(.*?)>"
+    $filesMoved++
+}
+$filesMoved--
+Write-Host "`nProcessed ($filesMoved) file(s).`n"
+Read-Host -Prompt “Press any key to exit”
